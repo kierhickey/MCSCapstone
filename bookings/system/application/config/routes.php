c194ad4920526275b9ec57f99b2d0127a02f868f
Aaron Marshall <azza_star851@hotmail.com>
2016-10-03 21:33:38 +1100

    Improves quality of routes.php
    This commit cleans up the routes.php file, adding documentation and order to the otherwise disordered file.

--- bookings/system/application/config/routes.php
+++ bookings/system/application/config/routes.php
@@ -1,67 +1,56 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-// Default CI stuff
-$route['default_controller']		= 'school';
-$route['scaffolding_trigger']		= "scaffolding";
+// Bookings
+$route['bookings']					= 'bookings/index';
+$route['bookings/summary']          = 'bookings/summary';
 
-
-// Classroom Bookings specific
-$route['site']						= 'site/home';
-$route['faq']						= 'site/faq';
+// ClassroomBookings-specific
 $route['contact']					= 'site/contact';
+$route['faq']						= 'site/faq';
 $route['features']					= 'site/features';
-$route['controlpanel']				= 'school/manage';
+$route['site']						= 'site/home';
 
+// School Management
+$route['controlpanel']				= 'school/manage';
+$route['default_controller']		= 'school';
+$route['school']					= 'school/manage';
+
+// Help
+$route['help']						= 'help/index';
+$route['help/:any']                 = 'help/index';
+$route['help/contents']				= 'help/contents';
+
+// Holidays
+$route['holidays']					= 'holidays/index';
+
+// Sessions
+$route['periods']					= 'periods/index';
+
+// Profile
+$route['profile']					= 'profile/index';
+$route['profile/(:num)']            = 'profile/index/$1';
+
+// Remote
+$route['remote']					= 'remote/index';
+
+// Reports
+$route['reports']					= 'reports/index';
 
 // Rooms
 $route['rooms']						= 'rooms/index';
 $route['rooms/fields']				= 'rooms/fields_index';
-$route['rooms/fields/save']		    = 'rooms/fields_save';
 $route['rooms/fields/add']		    = 'rooms/fields_add';
-
-$route['rooms/fields/edit']			= 'rooms/fields_edit';
-$route['rooms/fields/edit/:any']	= 'rooms/fields_edit';
-
 $route['rooms/fields/delete']		= 'rooms/fields_delete';
 $route['rooms/fields/delete/:any']	= 'rooms/fields_delete';
+$route['rooms/fields/edit']			= 'rooms/fields_edit';
+$route['rooms/fields/edit/:any']	= 'rooms/fields_edit';
+$route['rooms/fields/save']		    = 'rooms/fields_save';
 
-
-// Control panel options
-$route['weeks']						= 'weeks/index';
-$route['periods']					= 'periods/index';
-$route['holidays']					= 'holidays/index';
-$route['weekswizard']				= 'weekswizard/index';
-
-$route['school']					= 'school/manage';
-
-#$route['school/:any']							= 'school';
-#$route['school/deta'] = 'school/$1';
-
+// Miscellaneous
+$route['scaffolding_trigger']		= "scaffolding";
 $route['timetable']					= 'timetable/index';
 $route['users']						= 'users/index';
-$route['remote']					= 'remote/index';
-$route['reports']					= 'reports/index';
-$route['bookings']					= 'bookings/index';
-$route['profile']					= 'profile/index';
-$route['profile/(:num)']            = 'profile/index/$1';
-
-
-// Help ;-)
-$route['help']						= 'help/index';
-$route['help/contents']				= 'help/contents';
-#$route['help/:any']									= 'help/index';
-#$route['help/:any/:any']						= 'help/index';
-#$route['help/:any/:any/:any']				= 'help/index';
-#$route['help/:any/:any/:any/:any']	= 'help/index';
-#$route['help/(([a-z]+)[/]?)+'] = 'help/index/$1';
-$route['help/:any']                 = 'help/index';
-
-
-// Login & logout
-#$route['signup']							= 'signup/index';
-#$route['login/submit']				= 'login/submit';
-#$route['login/:any']					= 'login/index';		// login/<school-code> will allow people to login to that school
-#$route['([[:alnum:]]{3,10})']								= 'login/index/$1';		// as above
-
+$route['weeks']						= 'weeks/index';
+$route['weekswizard']				= 'weekswizard/index';
 
 ?>
