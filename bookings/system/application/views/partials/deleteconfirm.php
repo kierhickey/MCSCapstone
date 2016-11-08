6b0ac2aa8c54eb11ab6fd3e291d931f7c56aabad
samtimmins <stimmins@y7mail.com>
2016-11-08 18:36:40 +1100

    Delete verification screen small fix
    Both messages in red, both buttons are now actually buttons. Not Pretty, but works better :)

--- bookings/system/application/views/partials/deleteconfirm.php
+++ bookings/system/application/views/partials/deleteconfirm.php
@@ -1,12 +1,16 @@
 <?php echo form_open( $action, '', array( 'id' => $id ) ); ?>
 <br/>
-<p class="msgbox question">Are you sure you want to delete this item?</p>
+<p class="msgbox exclamation">Are you sure you want to delete this item?</p>
 <?php if( isset($text) ){ ?><p class="msgbox exclamation"><?php echo $text ?></p><?php } ?>
 <br /><br />
 <table cellpadding="0" cellspacing="0">
 	<tr>
 		<td align="left" valign="middle">
-			<?php echo form_submit( array( 'value' => 'Yes' ) ) ?> &nbsp;&nbsp;&nbsp; <?php echo anchor( $cancel, 'No') ?>
+			<?php echo form_submit( array( 'value' => 'Yes' ) ) ?> &nbsp;&nbsp;&nbsp; 
+            <input type="button" name="No" value="No" onClick="window.location=document.referrer"; />
+            
+            
+            <?php //echo anchor( $cancel, 'No') ?>
 		</td>
 	</tr>
 </table>
