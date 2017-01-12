95306650e73617de2e1b9d0e51ff3f29d83f5c18
Aaron Marshall <azza_star851@hotmail.com>
2017-01-12 22:49:56 +1100

    Adds more information to the legend on the bookings page.
    Adds information about past bookings/past recurring/past free slots on the bookings page.

--- bookings/system/application/views/bookings/key.php
+++ bookings/system/application/views/bookings/key.php
@@ -1,7 +1,13 @@
 <table border="0" cellpadding="4" cellspacing="4" class="bookings" align="center" style="border:1px solid #000;border-width:0px 0px;margin:16px auto;">
-<tr>
-<td width="25%" align="right" valign="middle"><strong>Legend:</strong></td>
-<td class="free" width="25%" align="center">Available session</td>
-<td class="casual" width="25%" align="center">Booking</td>
-<td class="static" width="25%" align="center">Recurring Booking</td>
-</tr></table>
+    <tr>
+        <td rowspan="2" width="25%" align="right" valign="middle"><strong>Legend:</strong></td>
+        <td class="free" width="25%" align="center">Available session</td>
+        <td class="casual" width="25%" align="center">Booking</td>
+        <td class="static" width="25%" align="center">Recurring Booking</td>
+    </tr>
+    <tr>
+        <td class="past-free" width="25%" align="center">Past session</td>
+        <td class="past-casual" width="25%" align="center">Past Booking</td>
+        <td class="past-static" width="25%" align="center">Past Recurring</td>
+    </tr>
+</table>
